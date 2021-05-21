c7b9bc2092d36c4ce0882556292bca7bc3bb1100
taturazova <urazovasmtl@gmail.com>
2021-05-21 11:23:53 -0700

    MergeSort method added

--- ListSort.py
+++ ListSort.py
@@ -9,6 +9,7 @@ Created on Fri May 21 09:39:36 2021
 
 list = [9, 11, 14, 25, 74, 23, 64, 99, 57, 86]
 
+
 # Feature #1 - Bubble Sort - Luke
 def feature1(input):
     #feature here
@@ -25,11 +26,46 @@ def feature3(input):
     return 
 
 # Feature #4 - Merge Sort - Tatiana
-def feature4(input):
+def mergeSort(input):
     #feature here
-    return 
+    if len(input)>1:
+        mid=len(input)//2
+        L=input[:mid]
+        R=input[mid:]
+
+        mergeSort(L)
+        mergeSort(R)
+
+        merge(input,L,R)
+    return
+# Helper merge method
+def merge(input,L,R):
+    i=j=k=0
+    while i<len(L) and j<len(R):
+        if L[i]<R[i]:
+            input[k]=L[i]
+            i+=1
+        else:
+            input[k]=R[j]
+            j+=1
+        k+=1
+    while i<len(L):
+        input[k]=L[i]
+        i+=1
+        k+=1
+    while j<len(R):
+        input[k]=R[j]
+        j+=1
+        k+=1
+    return
+# Print array method
+def printList(input):
+    for i in range(len(input)):
+        print(input[i],end=" ")
+    print()
 
 # Feature #5 - Insertion Sort - Tasha
 def feature5(input):
     #feature here
     return 
+
