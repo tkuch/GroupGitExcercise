976a9af46f237ed91f41258f24016d8f90033951
jkranabetter <jkranabetter@gmail.com>
2021-05-21 09:45:54 -0700

    Created Base List
    We will add feature to sort this list

---
+++ ListSort.py
@@ -0,0 +1,15 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Fri May 21 09:39:36 2021
+
+@author: jkran
+"""
+
+#Group 8 Git Practice Excersize
+
+list = [9, 11, 14, 25, 74, 23, 64, 99, 57, 86]
+
+# Feature #1
+def feature1(input):
+    #feature here
+    return 
