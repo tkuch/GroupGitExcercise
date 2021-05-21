c414d44a9bbc032f276fd2c8362083af75c7fead
jkranabetter <jkranabetter@gmail.com>
2021-05-21 09:49:28 -0700

    Update ListSort.py
    Added feature templates with responsibilities.

--- ListSort.py
+++ ListSort.py
@@ -9,7 +9,27 @@ Created on Fri May 21 09:39:36 2021
 
 list = [9, 11, 14, 25, 74, 23, 64, 99, 57, 86]
 
-# Feature #1
+# Feature #1 - Bubble Sort - Luke
 def feature1(input):
+    #feature here
+    return 
+
+# Feature #2 - Quick Sort - Joshua
+def feature2(input):
+    #feature here
+    return 
+
+# Feature #3 - Selection Sort - Devina
+def feature3(input):
+    #feature here
+    return 
+
+# Feature #4 - Merge Sort - Tatiana
+def feature4(input):
+    #feature here
+    return 
+
+# Feature #5 - Insertion Sort - Tasha
+def feature5(input):
     #feature here
     return 
