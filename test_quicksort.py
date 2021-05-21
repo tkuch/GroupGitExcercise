df0e702d284606e1ec543e15f290fa8b5324c12b
jkranabetter <jkranabetter@gmail.com>
2021-05-21 11:35:45 -0700

    Added Comment

--- test_quicksort.py
+++ test_quicksort.py
@@ -10,6 +10,7 @@ import unittest
 from ListSort import quickSort
 from ListSort import partition
 
+# basic test to see if quicksort is working
 class TestQuickSort(unittest.TestCase):
     def test_sort(self):
         self.assertEqual(quickSort([4,3,2,1], 0, 3), [1,2,3,4])
