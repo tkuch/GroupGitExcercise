c7bfbf456df1aa85e8cdd697b072cdbfb2acd326
Luke Sparg <lukesparg@hotmail.ca>
2021-05-24 15:32:40 -0700

    Added bubble sort and the related unit test.

--- ListSort.py
+++ ListSort.py
@@ -10,9 +10,22 @@ Created on Fri May 21 09:39:36 2021
 list = [9, 11, 14, 25, 74, 23, 64, 99, 57, 86]
 
 # Feature #1 - Bubble Sort - Luke
-def feature1(input):
-    #feature here
-    return 
+def bubbleSort(arr):
+    n = len(arr)
+  
+    # Traverse through all array elements
+    for i in range(n-1):
+    # range(n) also work but outer loop will repeat one time more than needed.
+  
+        # Last i elements are already in place
+        for j in range(0, n-i-1):
+  
+            # traverse the array from 0 to n-i-1
+            # Swap if the element found is greater
+            # than the next element
+            if arr[j] > arr[j+1] :
+                arr[j], arr[j+1] = arr[j+1], arr[j]
+    return arr
 
 # Feature #2 - Quick Sort - Joshua
 def partition(arr, low, high):
