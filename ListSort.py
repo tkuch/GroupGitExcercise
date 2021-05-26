9feb575864256dd3386da48d13c68af2f2c9939c
taturazova <urazovasmtl@gmail.com>
2021-05-26 11:34:02 -0700

    Testing file

--- ListSort.py
+++ ListSort.py
@@ -9,6 +9,10 @@ Created on Fri May 21 09:39:36 2021
 
 list = [9, 11, 14, 25, 74, 23, 64, 99, 57, 86]
 
+def printArr(input):
+    for i in range(len(input)):
+        print(input[i], end=' ')
+    return
 
 # Feature #1 - Bubble Sort - Luke
 def feature1(input):
@@ -26,23 +30,25 @@ def feature3(input):
     return 
 
 # Feature #4 - Merge Sort - Tatiana
-def mergeSort(input):
+def feature4(input):
     #feature here
     if len(input)>1:
         mid=len(input)//2
         L=input[:mid]
         R=input[mid:]
 
-        mergeSort(L)
-        mergeSort(R)
+        feature4(L)
+        feature4(R)
+
+      
 
         merge(input,L,R)
-    return
+    return input
 # Helper merge method
 def merge(input,L,R):
     i=j=k=0
     while i<len(L) and j<len(R):
-        if L[i]<R[i]:
+        if L[i]<R[j]:
             input[k]=L[i]
             i+=1
         else:
@@ -57,15 +63,16 @@ def merge(input,L,R):
         input[k]=R[j]
         j+=1
         k+=1
-    return
-# Print array method
-def printList(input):
-    for i in range(len(input)):
-        print(input[i],end=" ")
-    print()
+    return input
 
 # Feature #5 - Insertion Sort - Tasha
 def feature5(input):
     #feature here
     return 
+
+
+# Driver code for merge sort
+mergeArr = feature4(list)
+print("\nSorted array using smerge sort is:", end=' ')
+printArr(mergeArr)
 
--- README.md
+++ README.md
@@ -1,3 +1,6 @@
 # GroupGitExercise
 
 This repo is a group exercise for COSC499. We start with a list of integers and each develope a sorting feature for it.
+
+## MergeSort
+Merge sort is called using feature4(list), where list is an unsorted list, and returns a sorted list using the Merge sort algorithm running in O(nlogn) time
